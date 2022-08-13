8fa9e6dd19c5237d7d866674e116f3a0ce49999a
rickyurvina <ricky_uc12@hotmail.com>
2022-08-12 22:51:59 -0400

    Uso de checkbox

--- app/src/main/java/com/example/calculadoraapp/FirstFragment.java
+++ app/src/main/java/com/example/calculadoraapp/FirstFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +23,10 @@ public class FirstFragment extends Fragment {
     private TextView txt_res;
 
     private RadioButton rad_sum, rad_sub, rad_div, rad_mul;
+
+    private CheckBox chk_sum, chk_sub, chk_mul, chk_div;
+
+    private RadioGroup rad_group;
 
     private FragmentFirstBinding binding;
 
@@ -38,6 +44,11 @@ public class FirstFragment extends Fragment {
         rad_sub = (RadioButton) binding.radSub;
         rad_div = (RadioButton) binding.radDiv;
         rad_mul = (RadioButton) binding.radMul;
+        chk_sum = (CheckBox) binding.chkSum;
+        chk_sub = (CheckBox) binding.chkSub;
+        chk_mul = (CheckBox) binding.chkMul;
+        chk_div = (CheckBox) binding.chkDiv;
+        rad_group = (RadioGroup) binding.radGroup;
         return binding.getRoot();
     }
 
@@ -49,9 +60,24 @@ public class FirstFragment extends Fragment {
 //        });
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
         binding.btnSumar.setOnClickListener((View v) -> {
+            if (chk_sum.isChecked() || chk_sub.isChecked() || chk_mul.isChecked() || chk_div.isChecked()) {
+                String res = "";
+                uncheckRadios();
+                if (chk_sum.isChecked()) {
+                    res += " La suma es:" + sum() + " / ";
+                }
+                if (chk_sub.isChecked()) {
+                    res += " La resta es:" + sub() + " / ";
+                }
+                if (chk_mul.isChecked()) {
+                    res += " La mul es:" + mul() + " / ";
+                }
+                if (chk_div.isChecked()) {
+                    res += " La div es:" + div() + " / ";
+                }
+                txt_res.setText(res);
+            }
             if (rad_sum.isChecked()) {
                 sum();
             } else if (rad_sub.isChecked()) {
@@ -63,47 +89,60 @@ public class FirstFragment extends Fragment {
             } else {
                 showMessage();
             }
+
         });
+    }
+
+    public void uncheckRadios() {
+        rad_group.clearCheck();
     }
 
     public void showMessage() {
         Toast.makeText(this.getContext(), "No se ha seleccionado ninguna operación", Toast.LENGTH_SHORT).show();
     }
 
-    public void sum() {
+    public String sum() {
         double val1 = Integer.parseInt(txt_num1.getText().toString());
         double val2 = Integer.parseInt(txt_num2.getText().toString());
         double sum = val1 + val2;
         String res = String.valueOf(sum);
         txt_res.setText(res);
+        return res;
     }
 
-    public void sub() {
+    public String sub() {
         double val1 = Integer.parseInt(txt_num1.getText().toString());
         double val2 = Integer.parseInt(txt_num2.getText().toString());
         double sub = val1 - val2;
         String res = String.valueOf(sub);
         txt_res.setText(res);
+        return res;
+
     }
 
-    public void mul() {
+    public String mul() {
         double val1 = Integer.parseInt(txt_num1.getText().toString());
         double val2 = Integer.parseInt(txt_num2.getText().toString());
         double mul = val1 * val2;
         String res = String.valueOf(mul);
         txt_res.setText(res);
+        return res;
+
     }
 
-    public void div() {
+    public String div() {
         double val1 = Integer.parseInt(txt_num1.getText().toString());
         double val2 = Integer.parseInt(txt_num2.getText().toString());
         if (val2 != 0) {
             double sum = val1 / val2;
             String res = String.valueOf(sum);
             txt_res.setText(res);
-        } else {
-            Toast.makeText(this.getContext(), "No es posible dividir para 0", Toast.LENGTH_SHORT).show();
+            return res;
 
+        } else {
+            String message = "No es posible dividir para 0";
+            Toast.makeText(this.getContext(), message, Toast.LENGTH_SHORT).show();
+            return message;
         }
     }
 
